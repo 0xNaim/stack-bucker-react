385e968f2b18dd765aaa373ea6d45197b9c8ff66
0xNaim <naimislam3545@gmail.com>
2021-02-08 15:13:05 +0600

    Stack Bucker Day-1 Completed

---
+++ src/app/App.jsx
@@ -0,0 +1,22 @@
+import { Helmet } from "react-helmet";
+import AppRouter from "./router";
+
+function App() {
+  return (
+    <div className="App">
+      <Helmet>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;500&family=Roboto:wght@100;300;400;500;700&display=swap"
+          rel="stylesheet"
+        />
+      </Helmet>
+      <AppRouter />
+    </div>
+  );
+}
+
+export default App;
